f82aaa8a013523814d6283b796e7feb40da96c80
Pierre Labendzki <pierre.labendzki@gmail.com>
2020-01-02 20:36:07 +0100

    rajout de point virgule manquant

--- TP_template/main.cpp
+++ TP_template/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char** argv) {
 
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(tabPositions), tabPositions, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER,0)
+    glBindBuffer(GL_ARRAY_BUFFER,0);
     
 
     
@@ -67,7 +67,7 @@ int main(int argc, char** argv) {
 
     glGenVertexArrays(1,&vertexArrayID);
     glBindVertexArray(vertexArrayID);
-    glBindBuffer(GL_ARRAY_BUFFER,vbPositionsID)
+    glBindBuffer(GL_ARRAY_BUFFER,vbPositionsID);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),0);
     glBindBuffer(GL_ARRAY_BUFFER,0)
